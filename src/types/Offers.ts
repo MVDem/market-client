c6fb248e360baaf1c3cd5d94e74dc6e7a40e6070
mvdem <m9602598255@gmail.com>
2024-05-31 18:37:30 +0300

    Defined types of User, Farmer, Offer

---
+++ src/types/Offers.ts
@@ -0,0 +1,12 @@
+export type Offers = {
+  id: number;
+  unit: string;
+  price: string;
+  image: number;
+  isActive: boolean;
+  description_EN: string;
+  description_HE: string;
+  farmerId: number;
+  createdAt: string;
+  updatedAt: string;
+};
