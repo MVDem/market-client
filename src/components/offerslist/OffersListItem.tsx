7c1f3817688f8e4bb3c9b7badfcb0ad65ec73f2d
AlexeyT74 <alexey.tal@gmail.com>
2024-06-01 19:16:24 +0300

    added truncate utility function and aligned Price right side

--- src/components/offerslist/OffersListItem.tsx
+++ src/components/offerslist/OffersListItem.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './OffersListItem.module.scss';
 import { OfferCard } from '../../types/Offers';
+import { truncate } from '../../utils/truncate';
 
 interface OffersListItemProps {
   offer: OfferCard;
 }
 
-const language = 'he';
+const language = 'en';
+const maxDescLength = 100;
 
 const OffersListItem: React.FC<OffersListItemProps> = ({ offer }) => {
   const navigate = useNavigate();
@@ -25,17 +27,23 @@ const OffersListItem: React.FC<OffersListItemProps> = ({ offer }) => {
         className={styles.image}
       />
       <div className={styles.details}>
-        <h3 className={styles.unit}>{offer.unit}</h3>
-        <p className={styles.price}>${offer.price}</p>
-        <p className={styles.description}>
-          {language === 'en' ? offer.description_EN : offer.description_HE}
-        </p>
-        <p className={styles.farmer}>
-          Farmer:
-          <Link to={`/farmer/${offer.farmerId}`} className={styles.link}>
-            {offer.farmerId}
-          </Link>
-        </p>
+        <div className={styles.unitPrice}>
+          <h3 className={styles.unit}>{offer.unit}</h3>
+          <p className={styles.price}>${offer.price}</p>
+        </div>
+        <div className={styles.text}>
+          <p className={styles.description}>
+            {language === 'en'
+              ? truncate(offer.description_EN, maxDescLength)
+              : truncate(offer.description_HE, maxDescLength)}
+          </p>
+          <p className={styles.farmer}>
+            Farmer:
+            <Link to={`/farmer/${offer.farmerId}`} className={styles.link}>
+              {offer.farmerId}
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -41,7 +41,8 @@ export default function Home() {
       price: '50',
       image: 'https://images.unsplash.com/photo-1543158181-e6f9f6712055',
       isActive: true,
-      description_EN: 'Field strawberries',
+      description_EN:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit voluptatibus aliquid impedit facere beatae autem, sequi soluta nesciunt recusandae tempore quo, aliquam natus odio. Minus adipisci praesentium necessitatibus tempore cumque nam impedit optio porro saepe sunt autem, sapiente voluptate aut facilis ipsum animi sed consectetur. Quisquam vel ratione delectus earum culpa quam temporibus soluta expedita! Aliquid, unde doloribus deleniti officiis error nisi velit, soluta quidem distinctio dolore officia doloremque provident accusamus mollitia iure. Sit sapiente optio esse corporis architecto adipisci, dicta mollitia quasi voluptates dolores ad est quos consequatur praesentium dolore illo enim necessitatibus ea, ratione in? Inventore, ex hic?',
       description_HE: 'תותי שדה',
       farmerId: 67890,
     },
