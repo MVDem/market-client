992449e716446eefa1bec441ecc4e6fec26f5305
AlexeyT74 <alexey.tal@gmail.com>
2024-06-01 18:34:26 +0300

    Fixed the comments of Max

--- src/components/offerslist/OffersListItem.tsx
+++ src/components/offerslist/OffersListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './OffersListItem.module.scss';
 import { OfferCard } from '../../types/Offers';
 
@@ -7,31 +7,36 @@ interface OffersListItemProps {
   offer: OfferCard;
 }
 
-const language = 'en';
+const language = 'he';
 
 const OffersListItem: React.FC<OffersListItemProps> = ({ offer }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    const url = `/product/${offer.id}`;
+    console.log(url);
+    navigate(url);
+  };
+
   return (
-    <div className={styles.card}>
-      <Link to={`/product/${offer.id}`} className={styles.link}>
-        <img
-          src={offer.image}
-          alt={language === 'en' ? offer.description_EN : offer.description_HE}
-          className={styles.image}
-        />
-        <div className={styles.details}>
-          <h3 className={styles.unit}>{offer.unit}</h3>
-          <p className={styles.price}>${offer.price}</p>
-          <p className={styles.description}>
-            {language === 'en' ? offer.description_EN : offer.description_HE}
-          </p>
-          <p className={styles.farmer}>
-            Farmer:
-            <Link to={`/farmer/${offer.farmerId}`} className={styles.link}>
-              {offer.farmerId}
-            </Link>
-          </p>
-        </div>
-      </Link>
+    <div className={styles.card} onClick={handleClick}>
+      <img
+        src={offer.image}
+        alt={language === 'en' ? offer.description_EN : offer.description_HE}
+        className={styles.image}
+      />
+      <div className={styles.details}>
+        <h3 className={styles.unit}>{offer.unit}</h3>
+        <p className={styles.price}>${offer.price}</p>
+        <p className={styles.description}>
+          {language === 'en' ? offer.description_EN : offer.description_HE}
+        </p>
+        <p className={styles.farmer}>
+          Farmer:
+          <Link to={`/farmer/${offer.farmerId}`} className={styles.link}>
+            {offer.farmerId}
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,33 @@
 import Header from '../../components/header/Header';
 import OffersList from '../../components/offerslist/OffersList';
 import { OfferCard } from '../../types/Offers';
+import styles from './Home.module.scss';
 
 export default function Home() {
-  const offers:OfferCard[] = [
+  const offers: OfferCard[] = [
     {
       id: '1',
-      unit: 'kg',
+      unit: '3 kg',
       price: '10',
       image: 'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce',
       isActive: true,
       description_EN: 'Fresh apples',
-      description_HE: 'Fresh apples HE',
+      description_HE: 'תפוחים חדשים',
       farmerId: 12345,
     },
     {
       id: '2',
-      unit: 'kg',
+      unit: '1 kg',
       price: '15',
       image: 'https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b',
       isActive: true,
       description_EN: 'Organic oranges',
-      description_HE: 'Organic oranges HE',
+      description_HE: 'תפוזים חדשים',
       farmerId: 67890,
     },
     {
       id: '3',
-      unit: 'kg',
+      unit: '3 kg',
       price: '15',
       image: 'https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b',
       isActive: false,
@@ -34,13 +35,25 @@ export default function Home() {
       description_HE: 'Inactive HE',
       farmerId: 67890,
     },
+    {
+      id: '4',
+      unit: '100 g',
+      price: '50',
+      image: 'https://images.unsplash.com/photo-1543158181-e6f9f6712055',
+      isActive: true,
+      description_EN: 'Field strawberries',
+      description_HE: 'תותי שדה',
+      farmerId: 67890,
+    },
   ];
 
   return (
     <>
       <Header />
-      <h1>Home - Farmer Market</h1>
-      <OffersList offers={offers} />
+      <div className={styles.container}>
+        <h1>Home - Farmer Market</h1>
+        <OffersList offers={offers} />
+      </div>
     </>
   );
 }
