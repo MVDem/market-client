e802be407bb85c0eef7225e040ed622709784349
mvdem <m9602598255@gmail.com>
2024-06-02 19:45:44 +0300

    added Guard HOC

--- src/routes/Guard.tsx
+++ src/routes/Guard.tsx
@@ -1,14 +1,14 @@
-// import { useLocation, Navigate } from 'react-router-dom';
-// import { useAppSelector } from '../store/hooks';
+import { useLocation, Navigate } from 'react-router-dom';
+import { useAppSelector } from '../store/hooks';
 import { Role } from '../types/User';
 
 const Guard = ({ element, role }: { element: JSX.Element; role: Role }) => {
-  //   const location = useLocation();
-  //   const { user } = useAppSelector((state) => state.authReducer);
+  const location = useLocation();
+  const { user } = useAppSelector((state) => state.authReducer);
 
-  //   if (user.role !== role) {
-  //     return <Navigate to="/sign" state={{ from: location }} />;
-  //   }
+  if (user?.role !== role) {
+    return <Navigate to="/sign" state={{ from: location }} />;
+  }
   return element;
 };
 
