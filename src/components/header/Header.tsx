5adda40b5c9170a75f58c6ca319df6f934c93966
AndreevSV <andreevsv@gmail.com>
2024-06-06 19:34:11 +0300

    AvatarUI added to Header component. Done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@ import styles from './Header.module.scss';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { fetchLogout } from '../../store/thunks/auth.thunk';
 import { FiLogOut } from 'react-icons/fi';
+import AvatarUI from '../../UI/AvatarUI/AvatarUI';
 
 const Header: React.FC = () => {
   const { user } = useAppSelector((state) => state.authReducer);
@@ -23,18 +24,18 @@ const Header: React.FC = () => {
         <nav className={styles.navbar}>
           <ul>
             <li>
-              <NavLink to="/" className={setActive}>
+              <NavLink to='/' className={setActive}>
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={setActive}>
+              <NavLink to='/about' className={setActive}>
                 About
               </NavLink>
             </li>
             {user?.farmer && (
               <li>
-                <NavLink to="/farmer" className={setActive}>
+                <NavLink to='/farmer' className={setActive}>
                   My dashboard
                 </NavLink>
               </li>
@@ -49,11 +50,15 @@ const Header: React.FC = () => {
             <button onClick={handleLogout}>
               <FiLogOut />
             </button>
+            <AvatarUI src={user?.farmer?.imageURL ?? ''} />
           </div>
         ) : (
-          <NavLink to="/login" className={setActive}>
-            Login
-          </NavLink>
+          <>
+            <NavLink to='/login' className={setActive}>
+              Login
+            </NavLink>
+            <AvatarUI src='' />
+          </>
         )}
       </div>
     </div>
