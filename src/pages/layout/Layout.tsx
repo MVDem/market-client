3c29b511c8f18434314947b59811a64ace28f514
AndreevSV <andreevsv@gmail.com>
2024-06-09 10:53:08 +0300

    Fixing some errors after merge develop

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../../components/Header/Header';
-import SearchBar from '../../components/SearchBar/SearchBar';
+import SearchBar from '../../components/searchBar/SearchBar';
 import styles from './layout.module.scss';
 
 export default function Layout() {
