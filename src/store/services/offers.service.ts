b16657070605ee8bcec1d5bcf3f7119beaf6016b
AndreevSV <andreevsv@gmail.com>
2024-06-12 22:31:15 +0300

    RTK requests done

--- src/store/services/offers.service.ts
+++ src/store/services/offers.service.ts
@@ -1,24 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { API_URL } from '../../config';
-import { CreateOffer, Offer, OfferCard } from '../../types/Offers';
+import {
+  Offer,
+  CreateOfferUpload,
+  UpdateOfferUpload,
+} from '../../types/Offers';
 
 export const offersAPI = createApi({
   reducerPath: 'offersAPI',
   baseQuery: fetchBaseQuery({ baseUrl: `${API_URL}/offers` }),
   endpoints: (builder) => ({
     getPaginatedSortedOffers: builder.query<
-      { offers: OfferCard[]; count: number },
-      { limit?: number; page?: number; sortBy?: string; order?: string; search?: {columnName?: string; value?: string}} 
+      { offers: Offer[]; count: number },
+      {
+        limit?: number;
+        page?: number;
+        sortBy?: string;
+        order?: string;
+        search?: { columnName?: string; value?: string };
+      }
     >({
-      query: ({ limit = 10, page = 1, sortBy, order, search}) => {
+      query: ({ limit = 10, page = 1, sortBy, order, search }) => {
         const params = new URLSearchParams({
           limit: limit.toString(),
           page: page.toString(),
         });
         if (sortBy && sortBy.length) params.append('sortBy', sortBy);
         if (order && order.length) params.append('order', order);
-        if (search?.columnName && search.columnName.length) params.append('columnName', search.columnName);
-        if (search?.value && search.value.length) params.append('value', search.value);
+        if (search?.columnName && search.columnName.length)
+          params.append('columnName', search.columnName);
+        if (search?.value && search.value.length)
+          params.append('value', search.value);
 
         return {
           url: `?${params.toString()}`,
@@ -26,44 +38,48 @@ export const offersAPI = createApi({
         };
       },
     }),
-    getAllByFarmer: builder.query<Offer[], { farmerId: number }>({
-      query: ({ farmerId }) => ({
-        url: `/allByFarmer/${farmerId}`,
-        method: 'GET',
-      }),
+
+    create: builder.mutation<Offer, CreateOfferUpload>({
+      query: (body) => {
+        const headers = new Headers();
+        headers.append('Cookie', document.cookie);
+        const formData = new FormData();
+        formData.append('dto', JSON.stringify(body.dto));
+        if (body.file) {
+          formData.append('file', body.file);
+          headers.append('Content-Type', 'multipart/form-data');
+        }
+        return {
+          url: '',
+          method: 'POST',
+          body: formData,
+          headers: headers,
+        };
+      },
     }),
-    getFullOffers: builder.query<Offer[], number>({
-      query: () => ({
-        url: `/getfulloffers`,
-        method: 'GET',
-      }),
+
+    update: builder.mutation<Offer, UpdateOfferUpload>({
+      query: (body) => {
+        const headers = new Headers();
+        headers.append('Cookie', document.cookie);
+        const formData = new FormData();
+        if (body.dto) {
+          formData.append('dto', JSON.stringify(body.dto));
+        }
+        if (body.file) {
+          formData.append('file', body.file);
+          headers.append('Content-Type', 'multipart/form-data');
+        }
+
+        return {
+          url: '',
+          method: 'PUT',
+          body: formData,
+          headers: headers,
+        };
+      },
     }),
-    getOneById: builder.query<Offer, { offerId: string }>({
-      query: ({ offerId }) => ({
-        url: `/${offerId}`,
-        method: 'GET',
-      }),
-    }),
-    create: builder.mutation<Offer, CreateOffer>({
-      query: (body) => ({
-        url: '',
-        method: 'POST',
-        body,
-        headers: {
-          Cookie: document.cookie,
-        },
-      }),
-    }),
-    update: builder.mutation<Offer, CreateOffer>({
-      query: (body) => ({
-        url: '',
-        method: 'PUT',
-        body,
-        headers: {
-          Cookie: document.cookie,
-        },
-      }),
-    }),
+
     delete: builder.mutation<Offer, { offerId: number }>({
       query: ({ offerId }) => ({
         url: `/${offerId}`,
@@ -73,12 +89,20 @@ export const offersAPI = createApi({
         },
       }),
     }),
+
+    getById: builder.query<Offer, { offerId: number }>({
+      query: ({ offerId }) => ({
+        url: `/${offerId}`,
+        method: 'GET',
+      }),
+    }),
   }),
 });
 
 export const {
   useGetPaginatedSortedOffersQuery,
-  useGetAllByFarmerQuery,
-  useGetOneByIdQuery,
-  useGetFullOffersQuery,
+  useCreateMutation,
+  useUpdateMutation,
+  useDeleteMutation,
+  useGetByIdQuery,
 } = offersAPI;
