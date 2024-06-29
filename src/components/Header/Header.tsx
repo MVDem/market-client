bf4a3fb5905999e9656024030643a3e7c64ca901
mvdem <m9602598255@gmail.com>
2024-06-29 18:25:24 +0300

    Added Responsiveness, Created Dropdown Menu, Integrated Mobile Navbar Links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,60 +1,27 @@
-import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { fetchLogout } from '../../store/thunks/auth.thunk';
+import { useAppSelector } from '../../store/hooks';
 import { IoMenu } from 'react-icons/io5';
 import AvatarUI from '../../UI/AvatarUI/AvatarUI';
-import { Dropdown, MenuProps, Space } from 'antd';
+import DropDown from './DropDown';
 
-const Header: React.FC = () => {
+type DropDownProps = {
+  onCloseMemu?: () => void;
+};
+
+const Header: React.FC = ({}: DropDownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   const { user } = useAppSelector((state) => state.authReducer);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(fetchLogout());
-  };
-
-  const handleClick = () => {
-    navigate(`/profile/${user?.farmer?.id}`);
-  };
 
   const setActive = ({ isActive }: { isActive: boolean }) =>
     isActive ? styles.linkActive : styles.link;
-
-  const items: MenuProps['items'] = [
-    {
-      label: (
-        <button onClick={handleClick} className={styles.btn}>
-          My Profile
-        </button>
-      ),
-      key: '0',
-    },
-    {
-      label: (
-        <button onClick={handleLogout} className={styles.btn}>
-          Logout
-        </button>
-      ),
-      key: '1',
-    },
-  ];
 
   return (
     <div className={styles.header}>
       <div className={styles.container}>
         <div className={styles.leftgroup}>
-          <div className={styles.logo}>
-            <img
-              width="60"
-              height="60"
-              src="https://img.icons8.com/external-others-pike-picture/50/external-Village-tourism-others-pike-picture.png"
-              alt="external-Village-tourism-others-pike-picture"
-            />
-            <div>Farmers Market</div>
-          </div>
+          <div className={styles.logo}></div>
           <nav className={styles.navbar}>
             <ul>
               <li>
@@ -62,42 +29,28 @@ const Header: React.FC = () => {
                   Home
                 </NavLink>
               </li>
-              {/* <li>
-                <NavLink to="/about" className={setActive}>
-                  Farmers
-                </NavLink>
-              </li> */}
-              {/* {user?.farmer && (
-                <li>
-                  <NavLink to="/profile/1" className={setActive}>
-                    My Profile
-                  </NavLink>
-                </li>
-              )} */}
             </ul>
           </nav>
-          <div className={styles.burger}>
-            <IoMenu />
-          </div>
+        </div>
+        <div className={styles.burger} onClick={() => setIsOpen(!isOpen)}>
+          <IoMenu />
         </div>
         <div className={styles.rightPanel}>
           {user ? (
-            <Dropdown menu={{ items }} trigger={['click']}>
-              <a onClick={(e) => e.preventDefault()}>
-                <Space>
-                  <div className={styles.userInfo}>
-                    <h5>{user.email}</h5>
-                    <AvatarUI src={user.farmer?.logoURL!} size={40} />
-                  </div>
-                </Space>
-              </a>
-            </Dropdown>
+            <div
+              className={styles.rightMenu}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <h5>{user?.email}</h5>
+              <AvatarUI src={user?.farmer?.logoURL!} size={36} />
+            </div>
           ) : (
             <NavLink to="/sign" className={setActive}>
               Login
             </NavLink>
           )}
         </div>
+        {isOpen && <DropDown user={user} setIsOpen={setIsOpen} />}
       </div>
     </div>
   );
